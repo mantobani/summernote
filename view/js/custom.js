ef7069c8cb5ce54493d0e2c6268d2e5e3dd07918
mantobani <mantobani@gmail.com>
2018-02-02 16:35:46 +0000

    Update custom.js
    Añadimos las textareas de configuración de firma.

--- view/js/custom.js
+++ view/js/custom.js
@@ -4,4 +4,7 @@ $( document ).ready(function() {
 	if ($('#modal_enviar textarea').length) {
 		$('#modal_enviar textarea').attr('rows','').summernote({ height: 300 });
 	}
+	if ($('#panel_email #plantillas textarea').length) {
+		$('textarea[name^="mail_"]').attr('rows','').summernote({ height: 150 });
+	}
 });
