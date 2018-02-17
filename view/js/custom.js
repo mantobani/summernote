42f5cceab05a366991a0237fd590bd743dee7dee
mantobani <mantobani@gmail.com>
2018-02-17 20:04:37 +0000

    Update custom.js
    Añadimos selector de tamaños de fuente.

--- view/js/custom.js
+++ view/js/custom.js
@@ -2,9 +2,34 @@
 
 $( document ).ready(function() {
 	if ($('#modal_enviar textarea').length) {
-		$('#modal_enviar textarea').attr('rows','').summernote({ height: 300 });
+		$('#modal_enviar textarea').attr('rows','').summernote({ 
+			toolbar: [
+				['style', ['style']],
+				['fontsize', ['fontsize']],
+				['font', ['bold', 'italic', 'underline', 'clear']],
+				['fontname', ['fontname']],
+				['color', ['color']],
+				['para', ['ul', 'ol', 'paragraph']],
+				['height', ['height']],
+				['insert', ['picture', 'hr']],
+				['table', ['table']]
+			],		
+			height: 300
+		});
 	}
 	if ($('#panel_email #plantillas textarea').length) {
-		$('textarea[name^="mail_"]').attr('rows','').summernote({ height: 150 });
+		$('textarea[name^="mail_"]').attr('rows','').summernote({ 
+			toolbar: [
+				['style', ['style']],
+				['fontsize', ['fontsize']],
+				['font', ['bold', 'italic', 'underline', 'clear']],
+				['fontname', ['fontname']],
+				['color', ['color']],
+				['para', ['ul', 'ol', 'paragraph']],
+				['height', ['height']],
+				['insert', ['picture', 'hr']],
+				['table', ['table']]
+			],
+			height: 150 });
 	}
 });
